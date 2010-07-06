f929370979c050800cacf60f6f60ce80ca88d1d1
Chris Tandiono <chris.tandiono@gmail.com>
2010-07-06 15:59:56 -0700

    PDF cleaner; works by copying the pages of the old PDF to the new one

---
+++ cleanPDF.py
@@ -0,0 +1,18 @@
+from pyPdf import PdfFileWriter, PdfFileReader
+
+from sys import argv
+
+#arguments: argv1[1] is old pdf, argv[2] is new pdf
+
+output = PdfFileWriter()
+input1 = PdfFileReader(file(argv[1], "rb"))
+
+for i in range(0, input1.getNumPages()):
+    output.addPage(input1.getPage(i))
+
+outputStream = file(argv[2], "wb")
+output.write(outputStream)
+outputStream.flush()
+outputStream.close()
+
+
