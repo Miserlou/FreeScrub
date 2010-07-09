6217aaa46e55be49cbce250dcd0148e4509ea480
Michael McKinley <m.mckinley@gmail.com>
2010-07-08 21:38:13 -0500

    Minor style changes.

--- scrub/jpeg.py
+++ scrub/jpeg.py
@@ -14,7 +14,7 @@ def scrub(file_in, file_out):
     """
     Scrubs the jpeg file_in, returns results to file_out
     """
-    otmp = cStringIO.StringIO()
+    stripped = cStringIO.StringIO()
     with open(file_in, 'rb') as input_:
         header = input_.read(2)
         if header != '\xff\xd8':
@@ -24,14 +24,14 @@ def scrub(file_in, file_out):
             byte = input_.read(1)
             if len(byte) == 0:
                 break
-            if ord(byte) != 0xff:
-                otmp.write(byte)
+            if byte != '\xff':
+                stripped.write(byte)
             else:
-                byte2 = input_.read(1)
-                _get_handler(byte2)(input_, otmp)
+                next_byte = input_.read(1)
+                _get_handler(next_byte)(input_, stripped)
     with open(file_out, 'wb') as output:
-        output.write(otmp.getvalue())
-    otmp.close()
+        output.write(stripped.getvalue())
+    stripped.close()
 
 def _get_handler(byte):
     """
@@ -44,14 +44,13 @@ def _get_handler(byte):
             return _smart_copy_handler
         if msn == 0xd and lsn >= 0xb and lsn != 0xe:
             return _smart_copy_handler
-        return _copy_handler
-        
-    if msn == 0xe: #APPn segments
+        return lambda inp, out: _basic_handler(inp, out, True)
+    if msn == 0xe:
         return _app_handler
     if byte == '\x00' or byte == '\xff':
-        return lambda inp, out: out.write('\xff%s' % byte)
+        return lambda ___, out: out.write('\xff%s' % byte)
     
-    return _trash_handler 
+    return lambda inp, out: _basic_handler(inp, out, False) 
 
 def _app_handler(inp, out):
     """
@@ -63,8 +62,6 @@ def _app_handler(inp, out):
         length = _get_length(inp)
         inp.seek(length - 2, os.SEEK_CUR)
     
-        
-
 def _is_jfif(inp):
     """
     Determine if the data inp is currently seeked to indicates JFIF
@@ -79,25 +76,11 @@ def _is_jfif(inp):
     inp.seek(save_loc, os.SEEK_SET)
     return rval
     
-
 def _get_length(inp):
     """
     Get the length of the current frame (pointed to by inp)
     """
     return (ord(inp.read(1)) << 8) + ord(inp.read(1))
-
-def _trash_handler(inp, out):
-    """
-    Ignore the data until we reach a new marker
-    """
-    _basic_handler(inp, out, False)
-
-def _copy_handler(inp, out):
-    """
-    Copy the data to the output until we reach a new marker
-    """
-    _basic_handler(inp, out, True)
-
 
 def _basic_handler(inp, out, keep):
     """
@@ -111,20 +94,15 @@ def _basic_handler(inp, out, keep):
         byte = inp.read(1)
         if len(byte) == 0:
             break
-
-        #If we hit a potential marker, check if the next byte is 0x00 or
-        #0xff. If so, then it's not a marker. Otherwise, seek backwards
-        #and leave.
-        if ord(byte) == 0xff:
-            byte2 = inp.read(1)
-            if (0x00 < ord(byte2) < 0xff):
+        #Handle potential markers
+        if byte == '\xff':
+            next_byte = inp.read(1)
+            if (0x00 < ord(next_byte) < 0xff):
                 inp.seek(-2, os.SEEK_CUR)
                 return
-            #Only write out if we're keeping the data (e.g, this was
-            #called by _copy_handler
             elif keep:
                 out.write(byte)
-                out.write(byte2)
+                out.write(next_byte)
         elif keep:
             out.write(byte)
 
