7568efaa0609cd106ee2a39003a033adcac0b7bd
Rich Jones <rich@anomos.info>
2010-08-01 23:18:30 -0400

    winsetup

---
+++ winsetup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import py2exe
+import glob
+
+opts = {
+    "py2exe": {
+    "includes":"cairo, pango, pangocairo, atk, gobject"
+               ",encodings,encodings.*",
+    }
+}
+
+setup(windows=[{'script': 'scrubber.py',
+                "icon_resources": [(1, "icon.png")]}],
+      options=opts,
+      data_files=[(glob.glob("icon.png")) ],)
